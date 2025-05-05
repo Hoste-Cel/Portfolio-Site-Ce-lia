b273191fa55046ffdf082c9d27726e89dec78f48
Hoste-Cel <celiahoste@gmail.com>
2025-05-05 12:02:42 +0200

    Created JQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-alert("Hello");
+/* alert("Hello"); */
+
+
+$(document).ready(function () {
+    console.log("jQuery is working with CDN!");
+  });
+
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
